2f182b62fb6a3f45839b713c4fca25789fbc2dd9
Eskindir-Teshome <eskindirteshome@gmail.com>
2021-02-03 20:09:02 +0000

    "Hello world"

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
-
+/**
+*main - Entry point
+*
+*puts - print declared char string to std output
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  puts("\"Programming is like a building a multilingual puzzle");
+puts("\"Programming is like a building a multilingual puzzle");
 
-  return (0);
+return (0);
 }

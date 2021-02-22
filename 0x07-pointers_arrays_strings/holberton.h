f3451b90e787e81184a3e2e307fa4fbad0990857
Eskindir Teshome <eskindirteshome@gmail.com>
2021-02-22 16:42:52 +0000

    "More on arrays"

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,8 +1,8 @@
 #ifndef HOLBERTON_H
-#define HOLBERTON_H "holberton.h"
+#define HOLBERTON_H
 int _putchar(char c);
-char *memset(char *s, char b, unsigned int n);
-char *memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
